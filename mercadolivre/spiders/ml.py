c0616bfff3453841206afcc947bf595ecae7eae5
CASSIO  Veleda <VELEDC1@sa.ad.whirlpool.com>
2021-08-18 15:43:04 -0300

    primeiro commit

---
+++ mercadolivre/spiders/ml.py
@@ -0,0 +1,23 @@
+import scrapy
+
+
+class MlSpider(scrapy.Spider):
+    name = 'ml'
+
+    start_urls = ['http://mercadolivre.com.br/ofertas']
+
+    def parse(self, response, **kwargs):
+        for i in response.xpath('//li[@class="promotion-item"]'):
+            price = i.xpath('.//span[@class="promotion-item__price"]//text()').getall()
+            title = i.xpath('.//p[@class="promotion-item__title"]/text()').get()
+            link = i.xpath('.//a/href').get()
+
+            yield{
+                'price' : price, 
+            'title' : title, 
+            'link' : link
+            }
+
+            next_page = response.xpath('//a[contains(@title, "Próxima")]/@href').get()
+            if next_page:
+                yield scrapy.Request(url=next_page, callback=self.parse)
